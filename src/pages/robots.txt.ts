3944e55c9eb50359e5e03485119ceae8ae7d6cca
businessexpartbd-rgb <businessexpartbd@gmail.com>
2026-09-02 05:25:41 +0600

    Restore ASTRO-7 to pre-Dainik News state after migration

--- src/pages/robots.txt.ts
+++
@@ -1,4 +0,0 @@
-export function GET({ site }: { site: URL }) {
-  const origin = site ?? new URL('http://localhost:4321');
-  return new Response(`User-agent: *\nAllow: /\nSitemap: ${new URL('/sitemap.xml', origin)}\n`, { headers: { 'Content-Type': 'text/plain; charset=utf-8' } });
-}
